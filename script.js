f5ffc40118fbd0ad7f97c690432425c6cfd72b00
DantheDingoMan <90230698+DantheDingoMan@users.noreply.github.com>
2021-12-09 04:07:05 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,101 +3,47 @@
 /* jshint node: true */
 'use strict';
 
-//make sure to have pop up for empty id
+let base_url = "http://danthedingo.pythonanywhere.com/factioninfo/"
 
-const base_url = "https://danthedingo.pythonanywhere.com/api/v1/"
+async function nabinfo(factionname) {
 
-async function getjokes(category, language) {
-    let url = base_url + "jokes" + "?" + "category=" + category + '&' + "language=" + language
+    let url = base_url + factionname
     return fetch(url)
     .then(response => response.json())
+}
+
+async function info() {
+
+    var div = document.getElementById("info")
+    while (div.firstChild) {
+        div.removeChild(div.firstChild)
+    }
+    let factionname = document.getElementById('query').value
+    let info = document.createElement("p")
+
+    let infos = await nabinfo(factionname)
+
+    info.innerHTML = infos
+
+    div.appendChild(info)
+
 
 }
 
-async function getjoke(category, language, id) {
-    let url = base_url + "joke" + "?" + "category=" + category + '&' + "language=" + language + '&' + "id=" + id
-    return fetch(url)
-    .then(response => response.json())
+async function allinfo() {
 
-}
 
-async function jokes() {
-
-    var divs = document.getElementById("feedback")
-    while (divs.firstChild) {
-        divs.removeChild(divs.firstChild)
+    var div = document.getElementById("info")
+    while (div.firstChild) {
+        div.removeChild(div.firstChild)
     }
-    //needs to get value inside select not just select itself needs to only print out the number of jokes and does one slot per joke 
-    let category = document.getElementById('category').value
+    let factionname = ""
+    let info = document.createElement("p")
 
+    let infos = await nabinfo(factionname)
 
-    let language = document.getElementById('language').value
-    
-    let number = document.getElementById('number').value
+    info.innerHTML = infos
 
-    let numbers = parseInt(number)
+    div.appendChild(info)
 
-    let jokelist = []
-
-    for (let i = 0; i < numbers; i++) {
-        let joke = await getjokes(category, language)
-
-        jokelist.push(joke.joke)
-    }
-    
-
-    for (let i = 0; i < numbers; i++) {
-        let createitem = document.createElement("p")
-        createitem.setAttribute("class", "text-white")
-        createitem.innerHTML = jokelist[i]
-        divs.appendChild(createitem)
-    }
-
-}
-//make sure they need number
-
-async function popupkiller() {
-    let popup = document.getElementById('checker')
-
-    while (popup.firstChild) {
-        popup.removeChild(popup.firstChild);
-    }
-
-}
-
-async function joke() {
-    var divs = document.getElementById("feedback")
-
-    while (divs.firstChild) {
-        divs.removeChild(divs.firstChild)
-    }
-
-    let category = document.querySelector('#category').value
-
-    let language = document.querySelector('#language').value
-
-    let idnb = document.querySelector('#idnb').value
-
-    if (idnb == "") {
-        
-        let popup = document.getElementById('checker')
-        
-        let warning = document.createElement("p")
-        warning.setAttribute("class", "text-center bg-warning p-4")
-        warning.innerHTML = "You need a number"
-        window.setTimeout(popupkiller, 3000);
-
-        popup.appendChild(warning)
-        
-    }
-
-    let joke = await getjoke(category, language, idnb)
-
-    let divjokelist = joke.joke
-
-
-    let createitem = document.createElement("p")
-    createitem.setAttribute("class", "text-white")
-    createitem.innerHTML = divjokelist
-    divs.appendChild(createitem)
 }
